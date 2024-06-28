4150882d821790d9678b44b61c4fbad637e1153a
AnasBilal26 <anasbilalbhai@gmail.com>
2024-06-28 13:51:54 +0500

    project first

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import HI from './H1.jsx'
+
+function App() {
+  return (
+    <>
+    <div className="container">
+      <div className='second'>
+      <HI/>
+      <h1>My name is Anas Bilal</h1>
+      <h4>My First React App Project</h4>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
